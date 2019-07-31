b53fc5a62c7e8b75ea5b2cd9aa2d677d34fbd4c7
Edward Bryan Abergas <edwardbryan.abergas@gmail.com>
2019-07-31 15:00:31 +0800

    - Prepared Glide for future features

---
+++ app/src/main/java/bry1337/github/io/creditnotebook/injection/module/AppGlideModule.kt
@@ -0,0 +1,13 @@
+package bry1337.github.io.creditnotebook.injection.module
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by edwardbryan.abergas on 07/31/2019.
+ *
+ * @author edwardbryan.abergas@gmail.com
+ */
+@GlideModule
+class AppGlideModule : AppGlideModule() {
+}
